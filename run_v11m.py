e61f3a6b8d339ad9da959b928c5ed01ed44e33cb
Alexander Sigatchov <sa@inventos.ru>
2026-01-03 19:34:55 +0300

    test noms

--- run_v11m.py
+++ run_v11m.py
@@ -6,27 +6,28 @@ model = YOLO("yolo11m-pose.pt")  # Или yolo11x.pt для max точности
 data_yml = '/home/nssd/gled/vb/dataset-vb/vb-my-court/volleyball-court-keypoints-1/data.yaml'
 
 data_yml = '/home/nssd/gled/vb/dataset-vb/vb-my-court/vb-backline-mycams/yolo-datasets/data.yaml'
-results = model.train(
-     data=data_yml,
-
-     epochs=150,                  # Custom kp требует больше эпох
-     imgsz=1280,                  # Сохраняем детали корта
-     batch=4,                    # Auto-batch (или 8-16 вручную)
-     device=0,                    # GPU
-     patience=50,
-     augment=True,                # Критично для вариаций кортов (освещение, угол)
-     hsv_h=0.015, hsv_s=0.7, hsv_v=0.4,
-     degrees=20,
-     translate=0.2, 
-     scale=0.9, shear=10.0, fliplr=0.5,
-     mosaic=1.0, mixup=0.5,      # Сильные аугментации для generalization
-     optimizer="AdamW",
-     lr0=0.001,
-     close_mosaic=20,
-     name="volleyball_court_yolo11m_pose_mosaic_bl",
-     project="runs/yolo11m_pose",
-     exist_ok=True
- )
+data_yml = 'data/yolo-datasets/data.yaml'
+#results = model.train(
+#     data=data_yml,
+#
+#     epochs=150,                  # Custom kp требует больше эпох
+#     imgsz=1280,                  # Сохраняем детали корта
+#     batch=4,                    # Auto-batch (или 8-16 вручную)
+#     device=0,                    # GPU
+#     patience=50,
+#     augment=True,                # Критично для вариаций кортов (освещение, угол)
+#     hsv_h=0.015, hsv_s=0.7, hsv_v=0.4,
+#     degrees=20,
+#     translate=0.2, 
+#     scale=0.9, shear=10.0, fliplr=0.5,
+#     mosaic=1.0, mixup=0.5,      # Сильные аугментации для generalization
+#     optimizer="AdamW",
+#     lr0=0.001,
+#     close_mosaic=20,
+#     name="volleyball_court_yolo11m_pose_mosaic_bl",
+#     project="runs/yolo11m_pose",
+#     exist_ok=True
+# )
 
 model = YOLO("yolo11m-pose.pt")  # Или yolo11x.pt для max точности
 results = model.train(
@@ -42,10 +43,9 @@ results = model.train(
     hsv_v=0.4,
     degrees=10,
     translate=0.05, 
-    scale=0.5, shear=10.0,
+    scale=0.2, shear=5.0,
     fliplr=0.5,
-    
-    mosaic=0.3,
+    mosaic=0.0,
     close_mosaic=0,
     mixup=0.0,      # Сильные аугментации для generalization
     optimizer="AdamW",
@@ -58,7 +58,7 @@ results = model.train(
     warmup_epochs=3,
     
     
-    name="volleyball_court_yolo11m_pose_bl",
+    name="volleyball_court_yolo11m_pose_bl_work",
     project="runs/yolo11m_pose",
     exist_ok=True
 )
